3b0a6ce1a8893cf113d290897ed3aff843b2a5b1
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-01 18:49:45 +0100

    fix maps

--- src/replace_refs.php
+++ src/replace_refs.php
@@ -6,11 +6,13 @@ use Psl\Regex;
 use Psl\Str;
 
 const PODCAST_REGEX = '/<div class="podcast-embedded">.*<a href="([^"]+)".*'
-    . 'data-src="(https:\\/\\/oko.press\\/images\\/[^"]+)".*<h6[^>]*>([^<]+)<\\/h6>/';
+    . 'data-src="(https:\\/\\/oko\\.press\\/images\\/[^"]+)".*<h6[^>]*>([^<]+)<\\/h6>/';
 
 const ARTICLE_REGEX = '/<div class="powiazany-artykul-shortcode">.*<a href="([^"]+)".*'
-    . 'data-src="(https:\\/\\/oko.press\\/images\\/[^"]+)".*alt="([^"]+)".*'
+    . 'data-src="(https:\\/\\/oko\\.press\\/images\\/[^"]+)".*alt="([^"]+)".*'
     . '<h3[^>]*>([^<]+)<\\/h3>/';
+
+const MAP_REGEX = '/flourish-map.*data-url="([^"]+flourish\\.studio\\/[^"]+\\/embed)"/';
 
 /**
  * Replace references to other OKO.press articles and podcasts.
@@ -29,6 +31,10 @@ function replace_refs(string $source): string
             '<p><a href="%s">%s →</a></p> <img src="%s" alt="%s">',
             $article[1], $article[4], full_res($article[2]), $article[3],
         );
+    }
+
+    if (null !== $map = Regex\first_match($source, MAP_REGEX)) {
+        return Str\format('<p><a href="%s">Zobacz na mapie →</a></p>', $map[1]);
     }
 
     return $source;
