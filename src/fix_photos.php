e73269d1728ae847c267fac0de8cbf04254b0693
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-01 19:09:48 +0100

    fix more responsive photos

--- src/fix_photos.php
+++ src/fix_photos.php
@@ -5,17 +5,19 @@ namespace Major\OkoPrss;
 use Psl\Regex;
 use Psl\Str;
 
-const PHOTO_REGEX = '/data-srcset="(https:\\/\\/oko.press\\/images\\/[^"\\s]+)/';
-const PHOTO_PLACEHOLDER = 'src="https://oko.press/app/themes/oko/assets/images/lazy-archive-white.jpg"';
+const PHOTO_REGEX = '/<img.*src="https:\\/\\/[^"]+lazy-archive-white.jpg".*'
+    . 'data-srcset="(https:\\/\\/oko.press\\/images\\/[^"\\s]+)/';
+
+const PHOTO_PLACEHOLDER = 'https://oko.press/app/themes/oko/assets/images/lazy-archive-white.jpg';
 
 function fix_photos(string $source): string
 {
     if (
-        ! Str\starts_with($source, '<figure')
-        || null === $url = Regex\first_match($source, PHOTO_REGEX)
+        ! Str\contains($source, 'lazy-archive-white.jpg')
+        || null === $photo = Regex\first_match($source, PHOTO_REGEX)
     ) {
         return $source;
     }
 
-    return Str\replace($source, PHOTO_PLACEHOLDER, Str\format('src="%s"', $url[1]));
+    return Str\replace($source, PHOTO_PLACEHOLDER, $photo[1]);
 }
