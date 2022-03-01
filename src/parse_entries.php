2761891a74d4f0c96908613274cf197b951d71bb
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-01 18:00:45 +0100

    filter banners out

--- src/parse_entries.php
+++ src/parse_entries.php
@@ -3,6 +3,8 @@
 namespace Major\OkoPrss;
 
 use Psl\Iter;
+use Psl\Str;
+use Psl\Vec;
 use Symfony\Component\DomCrawler\Crawler;
 
 /**
@@ -14,6 +16,10 @@ function parse_entries(string $source): array
         ->filter('#banner-after-excerpt ~ div.entry-content')
         ->children()
         ->each(fn (Crawler $c) => $c);
+
+    $nodes = Vec\filter($nodes, function (Crawler $n): bool {
+        return ! Str\contains($n->html(), 'bannersData = bannersData[roulette];');
+    });
 
     [$entries] = Iter\reduce(
         $nodes,
