0098ee32b84bb8d1ed36f76dd3c48cdf9b333ad1
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-18 23:40:53 +0100

    add articles cache

--- src/get_source.php
+++ src/get_source.php
@@ -3,10 +3,34 @@
 namespace Major\OkoPrss;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psl\File;
+use Psl\Filesystem;
+use Psl\Str;
+
+const CACHE_PATH = __DIR__ . '/.cache/articles';
 
 function get_source(string $article): string
 {
-    $client = new Client(['base_uri' => 'https://oko.press', 'timeout' => 5.0]);
+    Filesystem\create_directory(CACHE_PATH);
 
-    return (string) $client->get($article)->getBody();
+    $cachePath = CACHE_PATH . '/' . $article;
+
+    $client = new Client(['base_uri' => 'https://oko.press', 'timeout' => 2.0]);
+
+    try {
+        $body = (string) $client->get($article)->getBody();
+
+        File\write($cachePath, $body);
+
+        return $body;
+    } catch (GuzzleException $e) {
+        if (! Filesystem\is_file($cachePath)) {
+            throw $e;
+        }
+
+        echo Str\format("Failed to fetch article %s, using cache.\n", $article);
+
+        return File\read($cachePath);
+    }
 }
