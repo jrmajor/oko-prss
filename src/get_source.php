efdac2d32611216fabfebf8c6daf073dbb5d2234
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-18 23:08:23 +0100

    increment request timeout

--- src/get_source.php
+++ src/get_source.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 
 function get_source(string $article): string
 {
-    $client = new Client(['base_uri' => 'https://oko.press', 'timeout' => 3.0]);
+    $client = new Client(['base_uri' => 'https://oko.press', 'timeout' => 5.0]);
 
     return (string) $client->get($article)->getBody();
 }
