315c4097a0907831b63afa0249702f5a4e87693d
Jeremiasz Major <jrh.mjr@gmail.com>
2022-04-07 15:14:04 +0200

    make fetching articles async

--- src/get_source.php
+++ src/get_source.php
@@ -2,34 +2,52 @@
 
 namespace Major\OkoPrss;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
+use Amp;
+use Amp\Http\Client\HttpClient;
+use Amp\Http\Client\Request;
+use Amp\Http\Client\TimeoutException;
+use Amp\Promise;
 use Psl\File;
 use Psl\Filesystem;
+use Psl\IO;
 
 const CACHE_PATH = __DIR__ . '/../.cache/articles';
 
-function get_source(int $n, string $article): string
+/**
+ * @return Promise<string>
+ */
+function get_source(HttpClient $client, int $n, string $article): Promise
 {
-    Filesystem\create_directory(CACHE_PATH);
+    return Amp\call(function () use ($client, $n, $article) {
+        Filesystem\create_directory(CACHE_PATH);
 
-    $cachePath = CACHE_PATH . "/{$n}-{$article}.html";
+        $cachePath = CACHE_PATH . "/{$n}-{$article}.html";
 
-    $client = new Client(['base_uri' => 'https://oko.press', 'timeout' => 2.0]);
+        $request = new Request("https://oko.press/{$article}");
 
-    try {
-        $body = (string) $client->get($article)->getBody();
+        $request->setInactivityTimeout(500);
+        $request->setTcpConnectTimeout(500);
+        $request->setTlsHandshakeTimeout(500);
+        $request->setTransferTimeout(500);
 
-        File\write($cachePath, $body);
+        try {
+            $response = yield $client->request($request);
 
-        return $body;
-    } catch (GuzzleException $e) {
-        if (! Filesystem\is_file($cachePath)) {
-            throw $e;
+            $body = yield $response->getBody()->buffer();
+
+            File\write($cachePath, $body);
+
+            IO\write_line('Fetched day %d.', $n);
+
+            return $body;
+        } catch (TimeoutException $e) {
+            if (! Filesystem\is_file($cachePath)) {
+                throw $e;
+            }
+
+            IO\write_line('Failed to fetch day %d, using cache.', $n);
+
+            return File\read($cachePath);
         }
-
-        echo "Failed to fetch day {$n}, using cache instead.\n";
-
-        return File\read($cachePath);
-    }
+    });
 }
